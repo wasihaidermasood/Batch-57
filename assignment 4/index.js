a6aa90377e8972dd9bf39881f16a0d86b17d55a2
wasi-haidermasood <hwasi1351@gmail.com>
2024-04-18 23:47:48 +0500

    assignment 4

---
+++ assignment 4/index.js
@@ -0,0 +1,32 @@
+"use strict";
+//1. Hello Variable
+let greeting = "Hello World";
+console.log(greeting);
+//2. Basic Math
+let num1, num2;
+num1 = 5;
+num2 = 10;
+console.log("Addition=", num1 + num2);
+console.log("Substraction=", num1 - num2);
+console.log("Multiplication=", num1 * num2);
+console.log("Division=", num1 / num2);
+//3. Swapping Values
+let a, b;
+a = 5;
+b = 12;
+a = a + b; //17
+b = a - b; //5
+a = a - b;
+console.log("a=", a);
+console.log("b=", b);
+//5. Modulus Operator
+let num3, num4;
+num3 = 10;
+num4 = 7;
+console.log("remainder=", num3 % num4);
+//6. Increment Challenge
+let q = 10;
+q++;
+console.log("prefix", q);
+++q;
+console.log("postfix", q);
